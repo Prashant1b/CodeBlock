c8aee60d2b094cd96ba8cbef03864df024da03e3
Prashant1b <Bhardwaj01b@gmail.com>
2026-02-22 17:11:56 +0530

    Add production URL to CORS configuration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -12,7 +12,7 @@
 
   app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://codeblock-frontend.onrender.com"],
     credentials: true,
   })
 );
